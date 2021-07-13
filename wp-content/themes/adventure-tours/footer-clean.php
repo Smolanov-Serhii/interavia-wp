559b033dd4f97e0f297e7e98795e718c48280f88
Sergey <smolanovsergey@gmail.com>
2021-07-13 14:29:43 +0300

    Весь адаптив

--- wp-content/themes/adventure-tours/footer-clean.php
+++ wp-content/themes/adventure-tours/footer-clean.php
@@ -13,8 +13,8 @@
 
 	<?php wp_footer(); ?>
 
-<script src="<?php echo get_template_directory_uri()?>/dist-cust/js/common.js"></script>
-<script src="<?php echo get_template_directory_uri()?>/src/js/jquery.selectric.js"></script>
+<script src="<?php echo get_template_directory_uri()?>/dist-cust/js/common2.js" defer></script>
+<script src="<?php echo get_template_directory_uri()?>/src/js/jquery.selectric.js" defer></script>
 
 </body>
 </html>
--- wp-content/themes/adventure-tours/header-clean.php
+++ wp-content/themes/adventure-tours/header-clean.php
@@ -12,11 +12,12 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<?php wp_head(); ?>
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/dist-cust/css/style.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/dist-cust/css/style2.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Karla&family=Playfair+Display:wght@700&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="https://code.jquery.com/ui/1.12.0/themes/smoothness/jquery-ui.css">
+    <script src="<?php echo get_template_directory_uri()?>/src/js/jquery-3.5.1.js"></script>
 </head>
 <body <?php body_class(); ?>>
 	<div class="layout-content">
